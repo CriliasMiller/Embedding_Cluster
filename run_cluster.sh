4f8aabe5eb29dcf364316b171ec1dda2895768b2
GakkiZ <74658737+CriliasMiller@users.noreply.github.com>
2025-01-18 18:18:09 +0800

    Update run_cluster.sh

--- run_cluster.sh
+++ run_cluster.sh
@@ -1,3 +1,3 @@
 source /root/miniconda3/etc/profile.d/conda.sh; 
 conda activate base;
-CUDA_VISIBLE_DEVICES=0,1,2,3,4,5,6,7 python kmean.py --clusters_num 10000 --iters 300 --npz_dir /workspace/intern/wangzehui/meta_manage/cluster_analysis/processed_npz_vertical
+CUDA_VISIBLE_DEVICES=0,1,2,3,4,5,6,7 python kmean.py --clusters_num 10000 --iters 300 --npz_dir
